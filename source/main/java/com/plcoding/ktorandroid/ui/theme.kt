6bea7dc2f5d1e06683642a5426e8855ad7f908ee
bdosch1 <131838710+bdosch1@users.noreply.github.com>
2023-04-25 22:57:53 -0500

    Update theme.kt

--- source/main/java/com/plcoding/ktorandroid/ui/theme.kt
+++ source/main/java/com/plcoding/ktorandroid/ui/theme.kt
@@ -1,17 +1,17 @@
 package com.plcoding.ktorandroidchat.ui.theme
-
+//import sdk
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
+//refers to color.kt
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
 )
-
+//refers to color.kt
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
@@ -26,7 +26,7 @@ private val LightColorPalette = lightColors(
     onSurface = Color.Black,
     */
 )
-
+//checks funtheme for dark mode
 @Composable
 fun KtorAndroidChatTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
